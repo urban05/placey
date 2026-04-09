877c0a541d0c2dab8c7d3db4d7474b6bfc1bc096
alien <alien040@t-online.de>
2026-04-09 11:19:54 +0200

    initialize nuxt

---
+++ app/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    <div class="text-red-500">Test</div>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+import tailwindcss from "@tailwindcss/vite"
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint', '@nuxt/icon', 'nuxt-maplibre'],
+  vite: {
+    plugins: [ tailwindcss() ]
+  }
+})
