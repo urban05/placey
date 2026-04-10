cb706029a984e0051acb2f4b4b9f1b8a69fe5e3e
Akira Pinemoon <paulik080705@gmail.com>
2026-04-10 12:03:51 +0200

    added vueuse fixed marker position anchor now using geolocation to place map

--- app/components/map.vue
+++ app/components/map.vue
@@ -2,26 +2,35 @@
 import { onMounted } from 'vue'
 import mapConfig from '@/assets/map_config.json';
 
+import { useGeolocation } from '@vueuse/core';
+
+const { coords } = useGeolocation();
+
 const maplibre = useMapLibre();
 
-const marker = useTemplateRef('marker');
+const markerElement = useTemplateRef('marker');
 
 onMounted(() => {
   const map = maplibre.createMap({
     container: 'map',
     style: mapConfig as any,
     center: [10.003277, 53.499555],
-    zoom: 2
+    zoom: 13
   })
 
-  maplibre.createMarker({ element: marker.value! }).setLngLat([10.003277, 53.499555]).addTo(map)
+  const marker = maplibre.createMarker({ element: markerElement.value!, anchor: 'bottom' });
+  marker.setLngLat([10.003277, 53.499555]).addTo(map)
+
+  watch(coords, (newCoords) => {
+    map.setCenter([newCoords.longitude, newCoords.latitude]);
+  })
 })
 </script>
 
 <template>
   <div id="map" style="height: 500px; width: 100%;">
     <div ref="marker">
-      <Icon name="glyphs-poly:graduation-cap" class="text-6xl drop-shadow-lg drop-shadow-black" />
+      <Icon name="twemoji:school" class="text-6xl drop-shadow-lg drop-shadow-black" />
     </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite"
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon'],
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@vueuse/nuxt'],
   vite: {
     plugins: [ tailwindcss() ]
   }
