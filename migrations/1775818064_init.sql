329d53c7ec90c444a479febd498bdecc2a8a0e9d
Paul Kunkel <paulik080705@gmail.com>
2026-04-10 14:33:58 +0200

    fixed migrations and naming

--- migrations/1775818064_init.sql
+++ migrations/1775818064_init.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS postgis;
 
-CREATE TABLE places (id UUID PRIMARY KEY, "name" VARCHAR, cords GEOGRAPHY, icon VARCHAR, verified BOOLEAN);
+CREATE TABLE places (id UUID PRIMARY KEY, "name" VARCHAR, cords GEOGRAPHY, icon VARCHAR, verified BOOLEAN, address VARCHAR, description VARCHAR);
 CREATE TABLE users (id UUID PRIMARY KEY, email VARCHAR UNIQUE, username VARCHAR UNIQUE);
 
-CREATE TABLE votes (place_id UUID REFERENCES places(id), user_id INTEGER REFERENCES users(id), vote INTEGER);
-CREATE TABLE users_place (place_id UUID REFERENCES places(id), user_id INTEGER REFERENCES users(id), first_visit TIMESTAMPTZ);
+CREATE TABLE votes (place_id UUID REFERENCES places(id), user_id UUID REFERENCES users(id), vote INTEGER);
+CREATE TABLE users_place (place_id UUID REFERENCES places(id), user_id UUID REFERENCES users(id), first_visit TIMESTAMPTZ);
---
+++ migrations/1775822747_insert_places.sql
@@ -0,0 +1,23 @@
+INSERT INTO PLACES (id, name, description, cords, icon, verified, address)
+VALUES
+   ('06e69402-a025-4a09-b08e-f01877c245eb', 'Bücherhalle Wilhelmsburg', 'Öffentliche Stadtteilbibliothek in Wilhelmsburg. Sie ist barrierearm erreichbar und hat Veranstaltungen', ST_MakePoint(15.606133, 48.105288), 'twemoji:books', true, 'Vogelhüttendeich 45, 21107 Hamburg'),
+   
+   ('3eb59bcc-e6e0-4571-ba7b-97b1d6ff4275', 'Wilhelmsburger Inselpark', 'Öffentliche Grünanlage mit Spiel, Sport und Freizeitflächen. Passt gut als frei zugänglicher Aufenthaltsort für Jugendliche und junge Erwachsene.', ST_MakePoint(10.002920, 53.495208), 'twemoji:deciduous-tree', true, 'Am Inselpark, 21109 Hamburg'),
+   
+   ('8c78f375-c41f-459a-bcc0-98a9e863b8de', 'Haus der Jugend Wilhelmsburg', 'Offene Jugendeinrichtung für 12 bis 27 Jahre, mit Jugendcafé, Billard, Konsole, Tonstudio, Fitnessraum und Sportflächen.', ST_MakePoint(9.991053, 53.511234), 'twemoji:people-holding-hands', true, 'Rotenhäuser Damm 58, 21107 Hamburg'),
+   
+   ('de4ec082-0be9-4181-af1f-9158144a9355', 'Haus der Jugend Kirchdorf-Süd', 'Offene Kinder und Jugendarbeit mit Freizeitangeboten, Beratung und Gruppenformaten.', ST_MakePoint(10.013570, 53.497596), 'twemoji:people-holding-hands', true, 'Krieterstraße 11, 21109 Hamburg'),
+   
+   ('a9b883fd-7949-4d21-82b9-7ca820a476c8', 'Honigfabrik Wilhelmsburg', 'Kulturzentrum in Wilhelmsburg mit Musik, Konzerten, Theater, Tanz, Werkstätten und Kinderkultur', ST_MakePoint(9.982788, 53.5158095), 'twemoji:performing-arts', true, 'Industriestraße 125-131, 21107 Hamburg'),
+   
+   ('74d139e2-7959-4d33-bc66-23e24ad8286f', 'SV Wilhelmsburg von 1888 e.V.', 'Großer Sportverein auf der Elbinsel mit breitem Sportangebot.', ST_MakePoint(10.0253387, 53.4868669), 'twemoji:soccer-ball', true, 'Karl-Arnold-Ring 15a, 21109 Hamburg'),
+   
+   ('84f3a4a7-9aa8-484c-afb7-c8d3dbea27bc', 'Bildungszentrum Tor zur Welt', 'Bildungsort im Herzen Wilhelmsburgs. Das Zentrum steht Bürgern jeden Alters und jeden Ausbildungsstandes offen.', ST_MakePoint(10.0124832, 53.5000361), 'twemoji:school', true, 'Krieterstraße 2d, 21109 Hamburg'),
+   
+   ('51e0b0e4-1e70-4547-a315-84ddb470463d', 'Schule An der Burgweide', 'Schule in Wilhelmsburg mit klar erkennbarem inklusiven Profil.', ST_MakePoint(10.023992, 53.48682), 'twemoji:school', true, 'Karl-Arnold-Ring 13, 21109 Hamburg'),
+   
+   ('aa9fd5d4-af49-4397-ae04-ecd3c4aa0469', 'Emmauskirche', 'Kirche in Wilhelmsburg mit vielfältigen Angeboten für Kinder, Jugendliche und Familien.', ST_MakePoint(9.9901507, 53.514347), 'twemoji:church', true, 'Eckermannstraße 1a, 21107 Hamburg'),
+   
+   ('82dbad2f-af27-4461-9548-16a8653eb0cd', 'Kreuzkirche Kirchdorf', 'Wilhelmsburger Kirche mit diversen Angeboten, sowohl für Kinder und Jugendliche als auch für Erwachsene.', ST_MakePoint(10.0110426, 53.5002946), 'twemoji:church', true, 'Jungnickelstraße 21, 21109 Hamburg'),
+   
+   ('2ddcb4d1-626a-42d8-a46f-1cf965a02f20', 'Berufliche Schule ITECH Elbinsel Wilhelmsburg', 'Inklusive Berufsschule auf der Elbinsel unterschiedlichsten Berufsausbildungen und Angeboten.', ST_MakePoint(10.0031441, 53.4996077), 'twemoji:school', true, 'Dratelnstraße 26, 21109 Hamburg');
