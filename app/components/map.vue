d210d46c4829171f6cf6de8e1d19faeaf73bae87
Paul Kunkel <paulik080705@gmail.com>
2026-04-14 14:50:09 +0200

    refactoplaces composable

--- app/components/map.vue
+++ app/components/map.vue
@@ -11,27 +11,6 @@ const popupRefs = ref<Record<string, HTMLElement | null>>({});
 
 const zoom = ref(13);
 
-const lastQueryLocation = ref<{ latitude: number; longitude: number }>({
-  latitude: userLocation.value.latitude,
-  longitude: userLocation.value.longitude,
-});
-
-function shouldQueryNewPlaces(newLocation: { latitude: number; longitude: number }) {
-  const distance = Math.abs(newLocation.latitude - lastQueryLocation.value.latitude) +
-    Math.abs(newLocation.longitude - lastQueryLocation.value.longitude);
-
-  return distance > 0.005;
-
-}
-
-const { data } = await useFetch(
-  () => `/api/places?latitude=${lastQueryLocation.value.latitude.toFixed(6)}&longitude=${lastQueryLocation.value.longitude.toFixed(6)}`, {
-  onResponse: (ctx) => {
-    places.value = ctx.response._data as any[];
-  }
-}
-);
-
 const map = ref<any>(null);
 const playerMarker = ref<maplibregl.Marker | null>(null);
 
@@ -47,21 +26,30 @@ onMounted(() => {
     zoom.value = map.value.getZoom()!;
   });
 
-  playerMarker.value = maplibre.createMarker({
-    color: "#FF0000"
-  }).setLngLat([userLocation.value.longitude, userLocation.value.latitude]).addTo(map.value);
-})
+  playerMarker.value = maplibre
+    .createMarker({
+      color: "#FF0000",
+    })
+    .setLngLat([userLocation.value.longitude, userLocation.value.latitude])
+    .addTo(map.value);
+});
 
 watch(
-  () => { return { latitude: userLocation.value.latitude, longitude: userLocation.value.longitude, heading: userLocation.value.heading } },
+  () => {
+    return {
+      latitude: userLocation.value.latitude,
+      longitude: userLocation.value.longitude,
+      heading: userLocation.value.heading,
+    };
+  },
   async (newLocation) => {
     map.value?.setCenter([newLocation.longitude, newLocation.latitude]);
     map.value?.setBearing(newLocation.heading);
-    playerMarker.value?.setLngLat([newLocation.longitude, newLocation.latitude]);
-    if (shouldQueryNewPlaces(newLocation)) {
-      lastQueryLocation.value = { latitude: newLocation.latitude, longitude: newLocation.longitude };
-    }
-  }
+    playerMarker.value?.setLngLat([
+      newLocation.longitude,
+      newLocation.latitude,
+    ]);
+  },
 );
 
 const markers = ref<maplibregl.Marker[]>([]);
@@ -75,28 +63,45 @@ watch(places, (newPlaces) => {
 
   // add new markers
   for (const place of newPlaces) {
-    const placePopup = maplibre.createPopup({ offset: 25, maxWidth: '100vw' }).setDOMContent(popupRefs.value[place.id]!);
-    maplibre.createMarker({
-      element: markerRefs.value[place.id]!,
-      anchor: "bottom",
-    }).setLngLat([place.longitude, place.latitude]).setPopup(placePopup).addTo(map.value!);
+    const placePopup = maplibre
+      .createPopup({ offset: 25, maxWidth: "100vw" })
+      .setDOMContent(popupRefs.value[place.id]!);
+    maplibre
+      .createMarker({
+        element: markerRefs.value[place.id]!,
+        anchor: "bottom",
+      })
+      .setLngLat([place.longitude, place.latitude])
+      .setPopup(placePopup)
+      .addTo(map.value!);
   }
 });
-
-
 </script>
 
 <template>
   <div id="map" style="height: 100vh; width: 100%">
-    <div v-for="place in places" :ref="(el) => (markerRefs[place.id] = el as HTMLElement)">
-      <Icon :name="place.icon" class="drop-shadow-lg drop-shadow-black cursor-pointer"
-        :size="40 * 1.2 ** (zoom - 13)" />
+    <div
+      v-for="place in places"
+      :ref="(el) => (markerRefs[place.id] = el as HTMLElement)"
+    >
+      <Icon
+        :name="place.icon"
+        class="drop-shadow-lg drop-shadow-black cursor-pointer"
+        :size="40 * 1.2 ** (zoom - 13)"
+      />
     </div>
 
-    <div v-for="place in places" :ref="(el) => (popupRefs[place.id] = el as HTMLElement)">
+    <div
+      v-for="place in places"
+      :ref="(el) => (popupRefs[place.id] = el as HTMLElement)"
+    >
       <div class="flex flex-col w-max">
-        <h1 class="text-xl text-nowrap whitespace-nowrap font-bold">{{ place.name }}</h1>
-        <p class="text-sm text-wrap break-normal my-3 w-0 min-w-full">{{ place.description }}</p>
+        <h1 class="text-xl text-nowrap whitespace-nowrap font-bold">
+          {{ place.name }}
+        </h1>
+        <p class="text-sm text-wrap break-normal my-3 w-0 min-w-full">
+          {{ place.description }}
+        </p>
         <p>Addresse:</p>
         <p>{{ place.address }}</p>
       </div>
--- app/composables/usePlaces.ts
+++ app/composables/usePlaces.ts
@@ -1,5 +1,44 @@
 import type { Place } from "~~/shared/place.type";
 
+const userLocation = useUserLocation();
+
+// store location of last data
+const lastQueryLocation = ref<{ latitude: number; longitude: number }>({
+  latitude: userLocation.value.latitude,
+  longitude: userLocation.value.longitude,
+});
+
+// check how far away the user is from the last query location
+function shouldQueryNewPlaces(newLocation: {
+  latitude: number;
+  longitude: number;
+}) {
+  const distance =
+    Math.abs(newLocation.latitude - lastQueryLocation.value.latitude) +
+    Math.abs(newLocation.longitude - lastQueryLocation.value.longitude);
+
+  return distance > 0.005;
+}
+
+// Watch for movement
+watch(
+  () => {
+    return {
+      latitude: userLocation.value.latitude,
+      longitude: userLocation.value.longitude,
+      heading: userLocation.value.heading,
+    };
+  },
+  (newLocation) => {
+    lastQueryLocation.value = newLocation;
+  },
+);
+
+const { data } = await useFetch(
+  () =>
+    `/api/places?latitude=${lastQueryLocation.value.latitude.toFixed(6)}&longitude=${lastQueryLocation.value.longitude.toFixed(6)}`,
+);
+
 export function usePlaces() {
-    return useState<Place[]>('places', () => [])
+  return data as Ref<Place[]>;
 }
