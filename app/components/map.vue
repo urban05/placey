8f496fc2465c389e0628eaf6fe185ba298129ace
alien <alien040@t-online.de>
2026-04-14 10:10:06 +0200

    add css to marker popup

--- app/components/map.vue
+++ app/components/map.vue
@@ -12,6 +12,7 @@ const { data, refresh } = await useFetch(
 const maplibre = useMapLibre();
 
 const markerRefs = ref<Record<string, HTMLElement | null>>({});
+const popupRefs = ref<Record<string, HTMLElement | null>>({});
 
 const zoom = ref(13);
 
@@ -31,29 +32,38 @@ onMounted(() => {
     map.setCenter([newCoords.longitude, newCoords.latitude]);
     refresh();
   });
-
   for (const place of data.value!) {
-    const placePopup = maplibre.createPopup({offset: 25}).setHTML(`<h1>${place.name}</h1><p>--------</p><p>${place.description}</p><p>--------</p><p>${place.address}</p>`);
     const placeMarker = maplibre.createMarker({
       element: markerRefs.value[place.id]!,
       anchor: "bottom",
-    });
-    placeMarker.setLngLat([place.longitude, place.latitude]).setPopup(placePopup).addTo(map);
+    }).setLngLat([place.longitude, place.latitude]);
+
+    const popupElem = popupRefs.value[place.id];
+    if (popupElem) {
+      const placePopup = maplibre.createPopup({ offset: 25, maxWidth: '100vw' }).setDOMContent(popupElem);
+      placeMarker.setPopup(placePopup);
+    }
+    placeMarker.addTo(map);
   }
 });
 </script>
 
 <template>
   <div id="map" style="height: 100vh; width: 100%">
-    <div
-      v-for="place in data"
-      :ref="(el) => (markerRefs[place.id] = el as HTMLElement)"
-    >
-      <Icon
-        :name="place.icon"
-        class="drop-shadow-lg drop-shadow-black cursor-pointer"
-        :size="40 * 1.2 ** (zoom - 13)"
-      />
+    <div v-for="place in data" :ref="(el) => (markerRefs[place.id] = el as HTMLElement)">
+      <Icon :name="place.icon" class="drop-shadow-lg drop-shadow-black cursor-pointer"
+        :size="40 * 1.2 ** (zoom - 13)" />
+    </div>
+
+    <div v-for="place in data" :ref="(el) => (popupRefs[place.id] = el as HTMLElement)">
+      <div class="flex flex-col w-max">
+        <h1 class="text-xl text-nowrap whitespace-nowrap font-bold">{{ place.name }}</h1>
+        <p class="text-sm text-wrap break-normal my-3 w-0 min-w-full">{{ place.description }}</p>
+        <p>Addresse:</p>
+        <p>{{ place.address }}</p>
+      </div>
     </div>
   </div>
 </template>
+
+<style lang="css" scoped></style>
