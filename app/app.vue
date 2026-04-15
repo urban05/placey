23f82f740b0191ce9c999e929023640badf41a1b
Akira Pinemoon <paulik080705@gmail.com>
2026-04-15 15:41:51 +0200

    added global svg for defining filters

--- app/app.vue
+++ app/app.vue
@@ -3,8 +3,10 @@
     <PlaceList class="hidden lg:block basis-2/5" />
     <Map class="lg:basis-full h-full" />
   </div>
+  <div v-html="filtersSvg"></div>
 </template>
 
 <script setup lang="ts">
 import "@/assets/css/index.css";
+import filtersSvg from '@/assets/filters.svg?raw'
 </script>
