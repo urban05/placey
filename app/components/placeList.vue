02bbaab06f3aed540f31fa3be03b7a862efb8e7e
Alien0002 <alien040@t-online.de>
2026-04-15 18:18:28 +0200

    issue #1

--- app/components/placeList.vue
+++ app/components/placeList.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="space-y-4 p-2 overflow-y-scroll">
-    <div v-for="place in places" :key="place.id" class="flex items-center gap-4 p-4 bg-white rounded-lg shadow">
-      <Icon :name="place.icon" size="60" :style="visitedPlaces.has(place.id) ? 'filter: url(\'#shimmer\')' : ''" />
-
-      <div class="flex-1">
-        <h3 class="font-semibold text-gray-900">{{ place.name }}</h3>
-        <p class="text-sm text-gray-600">{{ place.description }}</p>
-      </div>
-
-      <Voting :place="place" />
-    </div>
+    <PlaceListItem v-for="place in places" :key="place.id" />
   </div>
 </template>
 
