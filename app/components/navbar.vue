acc6c375fb8a1c0b857346a073557b8bff70ba8c
Paul Kunkel <paulik080705@gmail.com>
2026-04-16 12:04:22 +0200

    improved placeSearch css added buttons to navbar readded deleted placey-happy.webp

--- app/components/navbar.vue
+++ app/components/navbar.vue
@@ -1,6 +1,16 @@
 <template>
-  <nav class="flex flex-row justify-center p-2 gap-2">
+  <nav class="flex flex-row justify-center p-2 gap-2 h-16">
     <PlaceSearch class="w-full" />
+    <div
+      class="aspect-square bg-white shadow-lg rounded-full flex justify-center place-items-center"
+    >
+      <Icon name="twemoji:plus" size="30" />
+    </div>
+    <div
+      class="aspect-square bg-white shadow-lg rounded-full flex justify-center place-items-center"
+    >
+      <img src="../assets/placey-happy.webp" class="h-7.5 aspect-square" />
+    </div>
   </nav>
 </template>
 
--- app/components/placeSearch.vue
+++ app/components/placeSearch.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="relative max-w-xl">
+  <div class="relative max-w-xl h-full">
     <input
       @keyup.enter="zoomMapToFit"
       v-model="query"
-      class="bg-white rounded-full px-4 py-2 shadow-lg w-full"
+      class="bg-white h-full rounded-full px-4 shadow-lg w-full"
       placeholder="Search Places"
     />
     <Icon
       name="twemoji:magnifying-glass-tilted-right"
-      class="absolute right-2 top-1/2 -translate-y-1/2 grayscale pointer-events-none"
+      class="absolute right-3 top-1/2 -translate-y-1/2 grayscale pointer-events-none"
       size="30"
     />
   </div>
