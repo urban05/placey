7b416b617c563619156abb600b534e801bad4406
Alien0002 <alien040@t-online.de>
2026-04-17 11:40:50 +0200

    issue #3

--- app/components/placeList.vue
+++ app/components/placeList.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="space-y-4 p-2 overflow-y-scroll">
-    <PlaceListItem v-for="place in places" :key="place.id" :place="place" />
+    <template v-if="places && places.length != 0">
+      <PlaceListItem v-for="place in places" :key="place.id" :place="place" />
+    </template>
+    <button v-else class="flex items-center gap-4 p-4 w-full bg-white rounded-lg shadow" @click="onAddPlace">
+      <img src="~/assets/placey-sad.webp" width="42em">
+      <div class="flex-1">
+        <h3 class="font-semibold text-gray-900">Keine Ergebnisse?</h3>
+        <p class="text-sm text-gray-600">Füge einen neuen Ort hinzu!</p>
+      </div>
+    </button>
   </div>
+  
 </template>
 
 <script setup lang="ts">
 const places = usePlaces();
+
+function onAddPlace() {
+  console.log('Add Place');
+  // go to add place view
+}
 </script>
