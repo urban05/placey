8c856031407180285c68efaff77241750724fe98
Akira Pinemoon <paulik080705@gmail.com>
2026-04-17 09:32:38 +0200

    made navbar link to profile page

--- app/components/navbar.vue
+++ app/components/navbar.vue
@@ -1,19 +1,13 @@
 <template>
   <nav class="flex flex-row justify-center p-2 gap-2 h-16 md:px-24">
     <PlaceSearch class="w-full" />
-    <div
-      class="aspect-square bg-white shadow-lg rounded-full flex justify-center place-items-center"
-    >
+    <div class="aspect-square bg-white shadow-lg rounded-full flex justify-center place-items-center">
       <Icon name="twemoji:plus" size="30" />
     </div>
-    <div
-      class="aspect-square bg-white shadow-lg rounded-full flex justify-center place-items-center outline-4 outline-gray-200 md:absolute md:right-4 md:top-4 md:h-16"
-    >
-      <img
-        src="../assets/placey-happy.webp"
-        class="h-11 md:h-14 aspect-square"
-      />
-    </div>
+    <a href="/profile"
+      class="aspect-square bg-white shadow-lg rounded-full flex justify-center place-items-center outline-4 outline-gray-200 md:absolute md:right-4 md:top-4 md:h-16">
+      <img src="../assets/placey-happy.webp" class="h-11 md:h-14 aspect-square" />
+    </a>
   </nav>
 </template>
 
