6e355f08d37f7ff5dc5b6fa6d772e6bfd5bf4caf
Alien0002 <alien040@t-online.de>
2026-04-17 14:41:00 +0200

    issue #1

--- app/components/placeListItem.vue
+++ app/components/placeListItem.vue
@@ -1,13 +1,17 @@
 <template>
-  <button class="flex items-center gap-4 p-4 w-full bg-white rounded-lg shadow" @click="onPlaceClick">
-    <Icon :name="place.icon" size="60" :style="visitedPlaces.has(place.id) ? 'filter: url(\'#shimmer\')' : ''" />
-
-    <div class="flex-1">
-      <h3 class="font-semibold text-gray-900">{{ place.name }}</h3>
-      <p class="text-sm text-gray-600">{{ place.description }}</p>
+  <button class="flex items-center w-full bg-white rounded-lg shadow" @click="onPlaceClick">
+    <div class="grow relative">
+      <img class="absolute size-full object-cover object-center opacity-50 z-0" :src="place_image" />
+      <div class="flex flex-col gap-2 p-2 text-left">
+        <div class="flex gap-4 items-center z-2">
+          <Icon :name="place.icon" size="50" :style="visitedPlaces.has(place.id) ? 'filter: url(\'#shimmer\')' : ''" />
+          <h3 class="font-semibold text-gray-900" :style="text_border">{{ place.name }}</h3>
+          <Icon v-if="place.verified" name="twemoji:check-mark-button" size="20" />
+        </div>
+        <p class="text-sm text-gray-600" :style="text_border">{{ place.description }}</p>
+      </div>
     </div>
-
-    <Voting :place="place" />
+    <Voting :place="place" class="pr-2" />
   </button>
 </template>
 
@@ -17,8 +21,13 @@ import type { Place } from '~~/shared/place.type';
 const props = defineProps<{ place: Place }>();
 const visitedPlaces = useVisitedPlaces();
 const currentPlace = useCurrentPlace();
+const place_image = computed(() => props.place.image ?? "placeholder.svg");
+
+const tbs = 0.5; // text border size
+const text_border = computed(() => `text-shadow: ${tbs}px ${tbs}px 0px white, -${tbs}px -${tbs}px 0px white, ${tbs}px -${tbs}px 0px white, -${tbs}px ${tbs}px 0px white;`)
 
 function onPlaceClick() {
   currentPlace.value = props.place;
 }
 </script>
+
