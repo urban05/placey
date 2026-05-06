66459fea80724f3504f9d34e481db44447103402
Alien0002 <alien040@t-online.de>
2026-05-06 13:42:52 +0200

    added external link interception

---
+++ app/components/externalLink.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const { show } = useExternalLinkWarn();
+
+const props = defineProps<{ link: string }>();
+</script>
+
+<template>
+  <a
+    :href="link"
+    class="aspect-square size-8 relative rounded-full shadow-lg bg-white cursor-pointer flex justify-center items-center"
+    @click="(e: any) => {e.preventDefault(); show(link);}"
+  >
+    <slot />
+  </a>
+</template>
+
+<style>
+
+</style>
